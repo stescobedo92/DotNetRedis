f453d8bbdf1c81a71cb53a6b7334dc3bc56ba33b
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-01 11:06:57 -0600

    Add redis configuration

---
+++ DotNetRedis/RedisConnection.cs
@@ -0,0 +1,14 @@
+using StackExchange.Redis;
+
+namespace DotNetRedis;
+
+public class RedisConnection
+{
+    private static readonly Lazy<ConnectionMultiplexer> _lazyConnection;
+    private static ConnectionMultiplexer Connection => _lazyConnection.Value;
+    
+    static RedisConnection()
+    {
+        _lazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect("localhost"));
+    }
+}
