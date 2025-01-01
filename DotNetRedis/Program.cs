3696e107502151a859e8bc35822ec1e6cb1c7e5a
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-01 11:12:39 -0600

    Add more examples of interaction with redisDb

--- DotNetRedis/Program.cs
+++ DotNetRedis/Program.cs
@@ -3,6 +3,9 @@ using DotNetRedis;
 
 var redisDb = RedisConnection.Connection.GetDatabase();
 redisDb.StringSet("names", "Alice, Bob, Charlie");
+redisDb.StringSet("more_names", "Jhon, Doe, Lorem");
 
 var namesFromRedis = redisDb.StringGet("names");
+var moreNamesFromRedis = redisDb.StringGet("more_names");
 Console.WriteLine(namesFromRedis);
+Console.WriteLine(moreNamesFromRedis);
