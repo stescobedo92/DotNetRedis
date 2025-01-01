17b6a2969261289f1863c8831b4a14ddb07ebc5c
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-01 11:10:46 -0600

    Add interaction with redis database instance

--- DotNetRedis/Program.cs
+++ DotNetRedis/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using DotNetRedis;
 
-Console.WriteLine("Hello, World!");
+var redisDb = RedisConnection.Connection.GetDatabase();
+redisDb.StringSet("names", "Alice, Bob, Charlie");
+
+var namesFromRedis = redisDb.StringGet("names");
+Console.WriteLine(namesFromRedis);
